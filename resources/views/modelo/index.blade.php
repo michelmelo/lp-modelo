5fcfad0775c2b6e1ed1d557ae3dc706117d9ffec
michel melo <michellander@gmail.com>
2022-04-15 10:40:24 +0100

    init

---
+++ resources/views/modelo/index.blade.php
@@ -0,0 +1,276 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title  -->
+    <title>Portugal Boutique Winery - Guyot</title>
+
+    <!-- Favicon  -->
+    <link rel="icon" href="assets/img/favicon.png">
+
+    <!-- ***** All CSS Files ***** -->
+
+    <!-- Style css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+
+<body>
+    <!--====== Preloader Area Start ======-->
+    <div class="preloader-main">
+        <div class="preloader-wapper">
+            <svg class="preloader" xmlns="http://www.w3.org/2000/svg" version="1.1" width="600" height="200">
+                <defs>
+                    <filter id="goo" x="-40%" y="-40%" height="200%" width="400%">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -8" result="goo" />
+                    </filter>
+                </defs>
+                <g filter="url(#goo)">
+                    <circle class="dot" cx="50" cy="50" r="25" fill="#F74B54" />
+                    <circle class="dot" cx="50" cy="50" r="25" fill="#F74B54" />
+                </g>
+            </svg>
+            <div>
+                <div class="loader-section section-left"></div>
+                <div class="loader-section section-right"></div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== Scroll To Top Area Start ======-->
+    <div id="scrollUp" title="Scroll To Top">
+        <i class="fas fa-arrow-up"></i>
+    </div>
+    <!--====== Scroll To Top Area End ======-->
+
+    <div class="main">
+        <!-- ***** Header Start ***** -->
+        <header class="navbar navbar-sticky navbar-expand-lg navbar-dark">
+            <div class="container position-relative">
+                <a class="navbar-brand" href="index.html">
+                    <img class="navbar-brand-regular" src="assets/img/logo/logo.png" alt="brand-logo">
+                    <img class="navbar-brand-sticky" src="assets/img/logo/logo.png" alt="sticky brand-logo">
+                </a>           
+            </div>
+        </header>
+        <!-- ***** Header End ***** -->
+
+        <!-- ***** Welcome Area Start ***** -->
+        <section id="home" class="section welcome-area bg-inherit h-100vh overflow-hidden">
+            <div class="shapes-container">
+                <div class="bg-shape"></div>
+            </div>
+            <div class="container h-100">
+                <div class="row align-items-center h-100">
+                    <!-- Welcome Intro Start -->
+                    <div class="col-12 col-md-7">
+                        <div class="welcome-intro">
+                            <span class="d-inline-block text-capitalize font-italic fw-3 mb-2">Vinhos de Luxo</span>
+                            <!-- Portugal Boutique Winery - Guyot -->
+                            <h1 class="text-capitalize">Guyot <span class="fw-3">Branco</span> Portugal Boutique Winery</h1>
+                            <p class="my-4">Expressar a riqueza de um terroir único num vinho elegante e subtil.</p>
+                            <p>  Uvas provenientes de vinhas muito velhas, com idades superiores a 90 anos, em solos arenosos de origem granítica e a altitudes superiores a 500m.</p>
+                            <p>Field Blend, com mais de 20 castas presentes onde predominam a Códega e Rabigato.</p>
+                            <!-- Store Buttons -->
+                            <div class="button-group store-buttons d-flex">
+                                <a class="btn prolend-primary" href="https://pt.vinhosdeluxo.com/vinho/guyot">comprar</a>
+                                <a class="btn prolend-primary style-two" href="https://pt.vinhosdeluxo.com/brand/portugal-boutique-winery">Outros Vinhos</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5">
+                        <!-- Welcome Thumb -->
+                        <div class="welcome-thumb thumb-animated">
+                            <img src="assets/img/welcome/welcome-mockup.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ***** Welcome Area End ***** -->
+
+        <!-- ***** Promo Area Start ***** -->
+        <section id="products" class="section promo-area products-area overflow-hidden ptb_100 pt-lg-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4 res-margin">
+                        <div class="image-box product-card overflow-hidden">
+                            <img class="d-block w-100" src="assets/img/product-thumb/product-thumb-01.png" alt="">
+                            <div class="card-body p-4">
+                                <ul>
+                                    <li>Portugal Boutique Winery Boina Branco</li>
+<!--                                     <li>Smart listening experience by Adaptive Sound Control</li>
+                                    <li>Ergonomic, enfolding design earpads</li>
+ -->                                </ul>
+                            </div>
+                            <!-- Product Order -->
+                            <div class="product-order">
+                                <!-- Order Button -->
+                                <div class="order-btn d-flex flex-wrap justify-content-center">
+                                    <a class="btn prolend-primary style-two m-2" href="https://pt.vinhosdeluxo.com/vinho/guyot">comprar</a>
+                                    <a class="btn prolend-primary m-2" href="https://pt.vinhosdeluxo.com/brand/portugal-boutique-winery">Outros Vinhos</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 res-margin">
+                        <div class="image-box product-card overflow-hidden">
+                            <img class="d-block w-100" src="assets/img/product-thumb/product-thumb-02.png" alt="">
+                            <div class="card-body p-4">
+                                <ul>
+                                    <li>Portugal Boutique Winery Guyot Tinto Tinto</li>
+                                    <!-- <li>Smart listening experience by Adaptive Sound Control</li>
+                                    <li>Ergonomic, enfolding design earpads</li> -->
+                                </ul>
+                            </div>
+                            <!-- Product Order -->
+                            <div class="product-order">
+                                <!-- Order Button -->
+                                <div class="order-btn d-flex flex-wrap justify-content-center">
+                                    <a class="btn prolend-primary style-two m-2" href="hhttps://pt.vinhosdeluxo.com/vinho/guyot-3">comprar</a>
+                                    <a class="btn prolend-primary m-2" href="https://pt.vinhosdeluxo.com/brand/portugal-boutique-winery">Outros Vinhos</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="image-box product-card overflow-hidden">
+                            <img class="d-block w-100" src="assets/img/product-thumb/product-thumb-03.png" alt="">
+                            <div class="card-body p-4">
+                                <ul>
+                                    <li>Portugal Boutique Winery Loureiro Branco</li>
+                                    <!-- <li>Smart listening experience by Adaptive Sound Control</li>
+                                    <li>Ergonomic, enfolding design earpads</li> -->
+                                </ul>
+                            </div>
+                            <!-- Product Order -->
+                            <div class="product-order">
+                                <!-- Order Button -->
+                                <div class="order-btn d-flex flex-wrap justify-content-center">
+                                    <a class="btn prolend-primary style-two m-2" href="https://pt.vinhosdeluxo.com/vinho/gorro">comprar</a>
+                                    <a class="btn prolend-primary m-2" href="https://pt.vinhosdeluxo.com/brand/portugal-boutique-winery">Outros Vinhos</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ***** Promo Area End ***** -->
+
+
+
+        <!-- ***** Review Area Start ***** -->
+        <section class="review-area bg-gray ptb_100">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-10 col-lg-9">
+                        <!-- Section Heading -->
+                        <div class="section-heading text-center">
+                            <h2 class="text-capitalize">Um vinho que desafia os sentidos e que tem de ser provado!</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-12 col-lg-7">
+                        <div class="reviews owl-carousel">
+                            <!-- Single Review -->
+                            <div class="single-review text-center">
+                                <!-- Review Text -->
+                                <p class="review-text">
+                                    <img class="avatar mr-2" src="assets/img/icon/quote2.png" alt="">
+                                    Prova de boca de muito bom nível, fantástica acidez, banana verde, grainha de uva, maracujá, mas tudo com percepção de qualidade e complexidade. Um vinho que desafia os sentidos e que tem de ser provado!
+
+                                </p>
+                                <!-- Reviewer -->
+                                <div class="reviewer p-4">
+                                    <!-- Reviewer Thumb -->
+                                    <div class="reviewer-thumb">
+                                        <img class="avatar-lg mx-auto radius-100" src="https://images.vivino.com/avatars/default_user.png" alt="">
+                                    </div>
+                                    <!-- Reviewer Media -->
+                                    <div class="reviewer-meta mt-3">
+                                        <h5 class="reviewer-name mb-2">Jose Manuel Pereira</h5>
+                                        <h6 class="text-secondary fw-6">Portugal</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Single Review -->
+                            <div class="single-review text-center">
+                                <!-- Review Text -->
+                                <p class="review-text">
+                                    <img class="avatar mr-2" src="assets/img/icon/quote2.png" alt="">
+                                    Que bela surpresa. A expectativa era que fosse bom… mas não esperava tanto. Garrafa 6048 de 6056. Uma bonita cor amarela com laivos esverdeados. Nariz fabuloso e guloso de fruta de polpa branca, pêra madura, com frescura e mineralidade. Boca seca, excelente acidez, leve mas evoluído para umas notas como se tivesse madeira. Impressionou!
+                                </p>
+                                <!-- Reviewer -->
+                                <div class="reviewer p-4">
+                                    <!-- Reviewer Thumb -->
+                                    <div class="reviewer-thumb">
+                                        <img class="avatar-lg mx-auto radius-100" src="https://images.vivino.com/avatars/6_O2SefpSSyeN2z3k0EYhw.jpg" alt="">
+                                    </div>
+                                    <!-- Reviewer Media -->
+                                    <div class="reviewer-meta mt-3">
+                                        <h5 class="reviewer-name mb-2">Wine of Kings</h5>
+                                        <h6 class="text-secondary fw-6">Portugal</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Single Review -->
+                            <div class="single-review text-center">
+                                <!-- Review Text -->
+                                <p class="review-text">
+                                    <img class="avatar mr-2" src="assets/img/icon/quote2.png" alt="">
+                                    Primeiro vinho do encontro com os amigos Oscar e Hugo na Garrafeira de Santos. Vinho de boutique, pequena produção de apenas 6000 garrafas. Excelente branco do Douro, proveniente de vinhas centenárias plantadas em solo arenoso de origem granítica. Com muito frescor e mineralidade, possui ótima acidez. Um dos melhores no mercado. Saúde!🥂🥂
+                                </p>
+                                <!-- Reviewer -->
+                                <div class="reviewer p-4">
+                                    <!-- Reviewer Thumb -->
+                                    <div class="reviewer-thumb">
+                                        <img class="avatar-lg mx-auto radius-100" src="https://images.vivino.com/avatars/meDIaVU3T9GndRu54WAPjg.jpg" alt="">
+                                    </div>
+                                    <!-- Reviewer Media -->
+                                    <div class="reviewer-meta mt-3">
+                                        <h5 class="reviewer-name mb-2">Fred Pellucci</h5>
+                                        <h6 class="text-secondary fw-6">Brasil</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ***** Review Area End ***** -->
+
+
+
+    </div>
+
+
+    <!-- ***** All jQuery Plugins ***** -->
+
+    <!-- jQuery(necessary for all JavaScript plugins) -->
+    <script src="assets/js/jquery/jquery-3.3.1.min.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="assets/js/bootstrap/popper.min.js"></script>
+    <script src="assets/js/bootstrap/bootstrap.min.js"></script>
+
+    <!-- Plugins js -->
+    <script src="assets/js/plugins/plugins.min.js"></script>
+
+    <!-- Active js -->
+    <script src="assets/js/active.js"></script>
+</body>
+
+</html>
